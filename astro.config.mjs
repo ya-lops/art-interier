8c56ca68b57ed4936e91e8a66ee827806f155192
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-01-25 08:51:32 +0300

    upd config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: false,
-  scopedStyleStrategy: 'class',
-  trailingSlash: 'ignore',
-  build: {
-    assets: 'assets',
-    inlineStylesheets: 'never',
-  }
+  // compressHTML: false,
+  // scopedStyleStrategy: 'class',
+  // trailingSlash: 'ignore',
+  // build: {
+  //   assets: 'assets',
+  //   inlineStylesheets: 'never',
+  // }
 });
